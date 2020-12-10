d3c45219aeffd79c7d991c0a1ec2d2924043132b
Elise <elise.boes@student.ehb.be>
2020-12-10 11:23:09 +0100

    test(Helpers): check if yearly avarages property is an object

--- api/src/utils/helpers.js
+++ api/src/utils/helpers.js
@@ -5,6 +5,15 @@ const Helpers = {
   generateUUID: () => {
      const uuid = uuidv1();  
      return uuid;
+  },
+  //Check of het type van de maanden een object is en of de variabele niet null is
+  //En of het geen array is, omdat objecten en arrays verward worden
+  checkYearlyAverages: (months) =>{
+    if(typeof months === 'object' && months !== null && !Array.isArray(months)){
+      return months
+    }else{
+      return false
+    }
   }
 }
 
