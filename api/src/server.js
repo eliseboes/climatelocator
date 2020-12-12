11287380f96c5fa5e62020e310eab086ddac29ba
Elise <elise.boes@student.ehb.be>
2020-12-12 11:14:36 +0100

    fix: fix db connection & setup

--- api/src/server.js
+++ api/src/server.js
@@ -41,6 +41,45 @@ const pg = require('knex')({
   connection: process.env.PG_CONNECTION_STRING ? process.env.PG_CONNECTION_STRING : 'postgres://example:example@localhost:5432/climatelocator'
 });
 
+async function initialiseTables() {
+  await pg.schema.hasTable('year').then(async (exists) => {
+    if (!exists) {
+      await pg.schema
+        .createTable('year', (table) => {
+          table.increments();
+          table.uuid('uuid');
+          table.string('most_affected_country');
+          table.string('hottest_temperature');
+          table.string('num_disasters');
+          table.timestamps(true, true);
+        })
+        .then(async () => {
+          console.log('created table year');
+        });
+
+    }
+  });
+  await pg.schema.hasTable('locations').then(async (exists) => {
+    if (!exists) {
+      await pg.schema
+        .createTable('locations', (table) => {
+          table.increments();
+          table.uuid('uuid');
+          table.string('name');
+          table.string('geohash');
+          table.string('yearly_averages');
+          table.string('year');
+          table.string('year_id');
+          table.timestamps(true, true);
+        })
+        .then(async () => {
+          console.log('created table locations');
+        });
+    }
+  });
+}
+initialiseTables();
+
 app.post('/addlocation', async (req, res) => {
       res.status(201).send();
 });
