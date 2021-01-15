6d2f8ba3f278a8d32c1ab968156ca2fa3345d096
Elise <elise.boes@student.ehb.be>
2021-01-15 19:44:07 +0100

    test(end-to-end): test if location is not inserted when already exists

--- api/src/__tests__/integration/end-to-end.test.js
+++ api/src/__tests__/integration/end-to-end.test.js
@@ -4,54 +4,65 @@ const request = supertest(app);
 const Helpers = require('../../utils/helpers.js');
 
 describe('DB connection test', () => {
-    test('full connection test', async () => {
+    const uuid = Helpers.generateUUID();
+    const location = {
+        uuid: uuid,
+        name: 'San Fransisco',
+        geohash: 'c9gs1gzb4r26',
+        yearly_averages_low: {
+            Jan: 2.0,
+            Feb: 2.0,
+            Mar: 5.0,
+            Apr: 10.0,
+            May: 14.0,
+            Jun: 18.0,
+            Jul: 21.8,
+            Aug: 23.0,
+            Sep: 20.0,
+            Oct: 15.0,
+            Nov: 9.0,
+            Dec: 4.0
+        },
+        yearly_averages_high: {
+            Jan: 8.0,
+            Feb: 9.0,
+            Mar: 12.0,
+            Apr: 17.0,
+            May: 21.0,
+            Jun: 25.5,
+            Jul: 28.0,
+            Aug: 29.0,
+            Sep: 26.0,
+            Oct: 20.0,
+            Nov: 15.0,
+            Dec: 11.0
+        }
+    }
+
+    test('if location is added to the database if does not exsists', async () => {
         try {
-            let uuid = await request.post('/locations')
-                .send({
-                    uuid: Helpers.generateUUID(),
-                    name: 'San Fransisco',
-                    geohash: 'c9gs1gzb4r26',
-                    yearly_averages_low: {
-                        Jan: 2.0,
-                        Feb: 2.0,
-                        Mar: 5.0,
-                        Apr: 10.0,
-                        May: 14.0,
-                        Jun: 18.0,
-                        Jul: 21.8,
-                        Aug: 23.0,
-                        Sep: 20.0,
-                        Oct: 15.0,
-                        Nov: 9.0,
-                        Dec: 4.0
-                    },
-                    yearly_averages_high: {
-                        Jan: 8.0,
-                        Feb: 9.0,
-                        Mar: 12.0,
-                        Apr: 17.0,
-                        May: 21.0,
-                        Jun: 25.5,
-                        Jul: 28.0,
-                        Aug: 29.0,
-                        Sep: 26.0,
-                        Oct: 20.0,
-                        Nov: 15.0,
-                        Dec: 11.0
-                    }
-                })
-                .expect(201)
-                .then((response) => {
-                    const parsed = JSON.parse(response.text)
-                    return parsed.res[0].uuid
-                }).catch((error) => {
-                    console.log(error)
-                });
-            const getRequest = await request.get(`/locations/${uuid}`).expect(200)
-            const requestParse = JSON.parse(getRequest.text)
-            expect(requestParse.res).toHaveLength(1)
+            const response = await request.post('/locations').send(location)
+            expect(response.status).toBe(201)
+            expect(response.body[0]['id']).toBeDefined();
+            expect(response.body[0]['uuid']).toBeDefined();
+            expect(response.body[0]['name']).toBeDefined();
+            expect(response.body[0]['yearly_averages_high']).toBeDefined();
+            expect(response.body[0]['yearly_averages_low']).toBeDefined();
+            expect(response.body[0]['geohash']).toBeDefined();
+            uuid = response.body[0].uuid;
         } catch (error) {
             throw error
         }
     })
+    
+    test('if location is not added to the database if already exsists', async () => {
+        try {
+            const response = await request.post('/locations').send(location)
+            expect(response.status).toBe(404)
+            expect(response.body).toStrictEqual({})
+        } catch (error) {
+            throw error
+        }
+    })
+
 })
